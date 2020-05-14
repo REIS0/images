5e6c7aa2f9bf7c53a58f29996f9c4273157cfbf0
REIS0 <aor@cesar.school>
2020-05-14 00:27:01 -0300

    organize spaces

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,8 +4,7 @@ extern crate rand;
 
 use::std::io;
 // local functions
-use::images::bsc::create_gif;
-use::images::create_im::hori_vert_lines::animation_frames;
+use::images::{bsc::create_gif, create_im};
 
 const WIDTH: u32 = 500; // x
 const HEIGHT: u32 = 500; // y
@@ -15,11 +14,10 @@ fn main() {
     println!("Number of frames: ");
     let mut n_frames = String::new();
     io::stdin().read_line(&mut n_frames).expect("Failed to read line.");
-    let n_frames: i32 = n_frames.trim().parse().expect("Insert a valid integer value."); 
-
+    let n_frames: i32 = n_frames.trim().parse().expect("Insert a valid integer value.");
 
     println!("Creating frames...");
-    let frames = animation_frames(WIDTH, HEIGHT, n_frames);
+    let frames = create_im::random_squares::animation_frames(WIDTH, HEIGHT, n_frames);
 
     println!("Creating gif...");
     create_gif(WIDTH as u16, HEIGHT as u16, frames,"teste");
