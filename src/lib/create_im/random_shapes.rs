70cc00ce07be3600465d641881729bb17c0d29c6
REIS0 <aor@cesar.school>
2020-05-16 14:03:26 -0300

    Saving, need debug

--- src/lib/create_im/random_shapes.rs
+++ src/lib/create_im/random_shapes.rs
@@ -1,44 +1,45 @@
 use gif::Frame;
-use image::{imageops, RgbImage};
+use image::{imageops, RgbImage, Rgb};
 use rand::Rng;
 // local import
 use crate::bsc::generate_random_color;
-
-// TODO: debug
-fn draw_shape(width: u32, height: u32) -> RgbImage {
-    let color = generate_random_color();
-    let image = RgbImage::new(width, height);
-    image::Rgb([color.0, color.1, color.2]);
-    return image;
-}
 
 fn random_shape(image: &mut RgbImage) {
     let mut rng = rand::thread_rng();
     let image_width = image.dimensions().0;
     let image_height = image.dimensions().1;
-    let new_shape = draw_shape(
-        rng.gen_range(1, image_width),
-        rng.gen_range(1, image_height),
-    );
-    // image operations
-    imageops::overlay(
-        image,
-        &new_shape,
-        rng.gen_range(0, image_width),
-        rng.gen_range(0, image_height),
-    );
+    // DRAW shape
+    // generate random image coordinates
+    let shape_coord_x_begin = rng.gen_range(0, image_width);
+    let shape_coord_x_end = rng.gen_range(0, image_width);
+    let shape_coord_y_begin = rng.gen_range(0, image_height);
+    let shape_coord_y_end = rng.gen_range(0, image_height);
+    // generate shape color
+    let shape_color = generate_random_color();
+    // draw the new shape
+    for x in shape_coord_x_begin..shape_coord_x_end {
+        for y in shape_coord_y_begin..shape_coord_y_end {
+            image.put_pixel(
+                x,
+                y,
+                Rgb(shape_color)
+            )
+        }
+    }
 }
 
 pub fn animation_frames<'a>(width: u32, height: u32, n_frames: i32) -> Vec<Frame<'static>> {
     let mut frames: Vec<Frame> = Vec::new();
     let mut image = RgbImage::new(width, height);
+    // OPERATION
     for _i in 0..n_frames {
+        // TODO: checar essa logica
+        // TODO: debug
         let mut image_clone = image.clone();
         random_shape(&mut image_clone);
         // makes 'image' variable be the new rgbimage
         image = image_clone.clone();
         // CREATE FRAME
-        // TODO: checar panic
         let frame = Frame::from_rgb(width as u16, height as u16, &image_clone.into_vec());
         frames.push(frame);
     }
