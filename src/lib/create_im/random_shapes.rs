2d180a5ef4091963d42907e43fe63b342570c112
REIS0 <aor@cesar.school>
2020-08-01 17:22:47 -0300

    refactor random shape as struct

--- src/lib/create_im/random_shapes.rs
+++ src/lib/create_im/random_shapes.rs
@@ -1,41 +1,75 @@
-use gif::Frame;
-use image::{RgbImage, Rgb};
+use crate::generate_random_color;
+use crate::GifAnimation;
+use gif::{Encoder, Frame, Repeat, SetParameter};
+use image::{Rgb, RgbImage};
 use rand::Rng;
-// local import
-use crate::bsc::generate_random_color;
+use std::fs::File;
 
-fn random_shape(image: &mut RgbImage) {
-    let mut rng = rand::thread_rng();
-    let image_width = image.dimensions().0;
-    let image_height = image.dimensions().1;
-    // DRAW shape
-    // generate random image coordinates
-    let shape_coord_x_begin = rng.gen_range(0, image_width);
-    let shape_coord_x_end = rng.gen_range(shape_coord_x_begin, image_width);
-    let shape_coord_y_begin = rng.gen_range(0, image_height);
-    let shape_coord_y_end = rng.gen_range(shape_coord_y_begin, image_height);
-    // generate shape color
-    let shape_color = generate_random_color();
-    // draw the new shape
-    for x in shape_coord_x_begin..shape_coord_x_end {
-        for y in shape_coord_y_begin..shape_coord_y_end {
-            image.put_pixel(x, y, Rgb(shape_color));
+pub struct RandomShapesImage {
+    n_frames: i32,
+    width: u32,
+    height: u32,
+    rgb_image: RgbImage,
+}
+
+impl RandomShapesImage {
+    pub fn new(n_frames: i32, width: u32, height: u32) -> RandomShapesImage {
+        RandomShapesImage {
+            n_frames,
+            width,
+            height,
+            rgb_image: RgbImage::new(width, height),
+        }
+    }
+    fn random_shape(&mut self) {
+        let mut rng = rand::thread_rng();
+        // DRAW shape
+        // generate random image coordinates
+        let shape_coord_x_begin = rng.gen_range(0, self.width);
+        let shape_coord_x_end = rng.gen_range(shape_coord_x_begin, self.width);
+        let shape_coord_y_begin = rng.gen_range(0, self.height);
+        let shape_coord_y_end = rng.gen_range(shape_coord_y_begin, self.height);
+        // generate shape color
+        let shape_color = generate_random_color();
+        // draw the new shape
+        for x in shape_coord_x_begin..shape_coord_x_end {
+            for y in shape_coord_y_begin..shape_coord_y_end {
+                self.rgb_image.put_pixel(x, y, Rgb(shape_color));
+            }
         }
     }
 }
 
-pub fn animation_frames<'a>(width: u32, height: u32, n_frames: i32) -> Vec<Frame<'static>> {
-    let mut frames: Vec<Frame> = Vec::new();
-    let mut image = RgbImage::new(width, height);
-    // OPERATION
-    for _i in 1..n_frames {
-        let mut image_clone = image.clone();
-        random_shape(&mut image_clone);
-        // makes 'image' variable be the new rgbimage
-        image = image_clone.clone();
-        // CREATE FRAME
-        let frame = Frame::from_rgb(width as u16, height as u16, &image_clone.into_vec());
-        frames.push(frame);
+impl GifAnimation for RandomShapesImage {
+    fn animation_frames<'a>(&mut self) -> Vec<Frame<'static>> {
+        let mut frames: Vec<Frame> = Vec::new();
+        // OPERATION
+        for _i in 0..self.n_frames {
+            self.random_shape();
+            let frame = Frame::from_rgb(
+                self.width as u16,
+                self.height as u16,
+                // TODO: change clone to more optimized function
+                // ? multiple references?
+                &self.rgb_image.clone().into_vec(),
+            );
+            frames.push(frame);
+        }
+        frames
     }
-    frames
+
+    fn create_gif(&mut self, nome: &str) {
+        let frames = self.animation_frames();
+        let mut animation = File::create(nome.to_owned() + ".gif").unwrap();
+        let mut encoder = Encoder::new(
+            &mut animation,
+            self.width as u16,
+            self.height as u16,
+            &[0, 0, 0],
+        )
+        .unwrap();
+        encoder.set(Repeat::Infinite).unwrap();
+        // TODO: map filter
+        frames.iter().map(|f| encoder.write_frame(&f).unwrap());
+    }
 }
