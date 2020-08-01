feaec2509d213669bc9b088fb68ed0a82c095644
REIS0 <aor@cesar.school>
2020-08-01 17:22:37 -0300

    refactor lib functions and traits

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,2 +1,24 @@
+use gif::Frame;
+use rand::Rng;
+
 pub mod create_im;
-pub mod bsc;
+
+macro_rules! rng {
+    () => {
+        rand::thread_rng().gen();
+    };
+}
+
+// return a RGB array
+pub fn generate_random_color() -> [u8; 3] {
+    let red: u8 = rng!();
+    let green: u8 = rng!();
+    let blue: u8 = rng!();
+    [red, green, blue]
+}
+
+pub trait GifAnimation {
+    fn create_gif(&mut self, nome: &str);
+
+    fn animation_frames<'a>(&mut self) -> Vec<Frame<'static>>;
+}
