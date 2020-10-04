2eca35196334af564a71309450ce65f08d083b67
REIS0 <aor@cesar.school>
2020-10-04 15:06:17 -0300

    Add random image tests

---
+++ tests/random_image_test.rs
@@ -0,0 +1,9 @@
+use images::{GifAnimation, create_im::random_shapes};
+
+#[test]
+fn create_gif() {
+    let mut im = random_shapes::RandomShapesImage::new(5, 250, 100);
+    im.create_gif("test.gif");
+}
+
+
