e8e99511fea0906e7edc7e8b69473f1d162b33ed
REIS0 <aor@cesar.school>
2020-10-05 00:15:15 -0300

    refactor hori vert lines

--- src/lib/create_im/hori_vert_lines.rs
+++ src/lib/create_im/hori_vert_lines.rs
@@ -1,49 +1,87 @@
-use gif::Frame;
+use std::fs::File;
+
+use gif::{Encoder, Frame, Repeat, SetParameter};
 use image::{Rgb, RgbImage};
 // local import
-use crate::bsc::generate_random_color;
+use crate::{generate_random_color, GifAnimation};
 
-// TODO: function to return image buffer
-fn image_vec(width: u32, height: u32) -> Vec<u8> {
-    // CREATE IMAGE
-    let mut image = RgbImage::new(width, height);
-    // horizontal lines
-    for y in 0..(height / 2) {
-        let color = generate_random_color();
-        for x in 0..(width / 2) {
-            image.put_pixel(x, y, Rgb(color));
-        }
-    }
-    for y in (height / 2)..height {
-        let color = generate_random_color();
-        for x in (width / 2)..width {
-            image.put_pixel(x, y, Rgb(color));
-        }
-    }
-    // vertical lines
-    for x in 0..(width / 2) {
-        let color = generate_random_color();
-        for y in (height / 2)..height {
-            image.put_pixel(x, y, Rgb(color));
-        }
-    }
-    for x in (width / 2)..width {
-        let color = generate_random_color();
-        for y in 0..(height / 2) {
-            image.put_pixel(x, y, Rgb(color));
-        }
-    }
-    image.into_vec()
+// ? talvez usar u16 inves de u32
+pub struct HoriVertLines {
+    n_frames: i32,
+    width: u32,
+    height: u32,
+    rgb_image: RgbImage,
 }
 
-pub fn animation_frames<'a>(width: u32, height: u32, n_frames: i32) -> Vec<Frame<'static>> {
-    let mut frames: Vec<Frame> = Vec::new();
-    // TODO: otimizar e talvez paralelizar
-    for _i in 0..n_frames {
-        let image_vec = image_vec(width, height);
-        // CREATE FRAME
-        let frame = Frame::from_rgb(width as u16, height as u16, &image_vec);
-        frames.push(frame);
+impl HoriVertLines {
+    pub fn new(n_frames: i32, width: u32, height: u32) -> HoriVertLines {
+        HoriVertLines {
+            n_frames,
+            width,
+            height,
+            rgb_image: RgbImage::new(width, height),
+        }
     }
-    frames
+
+    fn image_vec(&mut self) {
+        // horizontal lines
+        for y in 0..(self.height / 2) {
+            let color = generate_random_color();
+            for x in 0..(self.width / 2) {
+                self.rgb_image.put_pixel(x, y, Rgb(color));
+            }
+        }
+        for y in (self.height / 2)..self.height {
+            let color = generate_random_color();
+            for x in (self.width / 2)..self.width {
+                self.rgb_image.put_pixel(x, y, Rgb(color));
+            }
+        }
+        // vertical lines
+        for x in 0..(self.width / 2) {
+            let color = generate_random_color();
+            for y in (self.height / 2)..self.height {
+                self.rgb_image.put_pixel(x, y, Rgb(color));
+            }
+        }
+        for x in (self.width / 2)..self.width {
+            let color = generate_random_color();
+            for y in 0..(self.height / 2) {
+                self.rgb_image.put_pixel(x, y, Rgb(color));
+            }
+        }
+    }
+}
+
+impl GifAnimation for HoriVertLines {
+    fn create_gif(&mut self, nome: &str) {
+        let frames = self.animation_frames();
+        let mut animation = File::create(nome).unwrap();
+        let mut encoder = Encoder::new(
+            &mut animation,
+            self.width as u16,
+            self.height as u16,
+            &[0, 0, 0],
+        )
+        .unwrap();
+        encoder.set(Repeat::Infinite).unwrap();
+        for frame in frames {
+            encoder.write_frame(&frame).ok();
+        }
+    }
+
+    fn animation_frames<'a>(&mut self) -> Vec<Frame<'static>> {
+        let mut frames: Vec<Frame> = Vec::new();
+
+        for _i in 0..self.n_frames {
+            self.image_vec();
+            let frame = Frame::from_rgb(
+                self.width as u16,
+                self.height as u16,
+                &self.rgb_image.clone().into_vec(),
+            );
+            frames.push(frame);
+        }
+        frames
+    }
 }
