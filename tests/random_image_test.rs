f81cfc5ce24fae5cc212fe4438adf862c64e2ba6
REIS0 <aor@cesar.school>
2020-10-05 00:12:46 -0300

    change gif name

--- tests/random_image_test.rs
+++ tests/random_image_test.rs
@@ -3,7 +3,7 @@ use images::{GifAnimation, create_im::random_shapes};
 #[test]
 fn create_gif() {
     let mut im = random_shapes::RandomShapesImage::new(5, 250, 100);
-    im.create_gif("test.gif");
+    im.create_gif("random_shape_test.gif");
 }
 
 
